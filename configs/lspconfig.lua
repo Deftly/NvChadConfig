676b0f6966a232528458636405f5dda0ce9e58c6
Deftly <deftkz@gmail.com>
2023-05-08 20:12:07 -0700

    lsp config for terramform and markdown

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -9,7 +9,7 @@ lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   cmd = {"gopls"},
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
     gopls = {
@@ -20,6 +20,24 @@ lspconfig.gopls.setup {
       },
     },
   },
+}
+
+-- Markdown
+lspconfig.marksman.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"marksman", "server"},
+  filetypes = {"markdown"},
+  root_dir = util.root_pattern(".git", ".marksman.toml"),
+}
+
+-- Terraform 
+lspconfig.terraformls.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"terraform-ls", "serve"},
+  filetypes = {"terraform", "terraform-vars"},
+  root_dir = util.root_pattern(".terraform", ".git"),
 }
 -- local servers = {"html", "cssls", "gopls", "marksman"}
 --
